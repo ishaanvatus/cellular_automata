69a8e85508c276ecff7c721f6c0e65aa49085992
ishaanvatus <ishaanvatus@gmail.com>
2022-10-03 02:26:21 +0530

    shell script is simpler and more verbose, mkv's are now outputted

--- conways_game_of_life/animate.sh
+++ conways_game_of_life/animate.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 ./conways_game_of_life $(($1 / $3)) $(($2 / $3)) $4
-
 ffmpeg -f image2 -i %05d.pbm -vf "scale=$1x$2:flags=neighbor,fps=24" -c:v libx264 -tune animation -preset medium "conways_game_of_life.mkv" 
+rm *.pbm
 ffmpeg -hide_banner -loglevel error -i "heatmap.pgm" -vf "scale=$1x$2:flags=neighbor" "heatmap.png"
 rm "heatmap.pgm"
-rm *.pbm
