4caa6d743a33c883f9637c82e647164155c52c49
ishaanvatus <ishaanvatus@gmail.com>
2022-10-03 01:57:43 +0530

    simplified animate shell script

--- conways_game_of_life/animate.sh
+++ conways_game_of_life/animate.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-
 ./conways_game_of_life $(($1 / $3)) $(($2 / $3)) $4
-for f in *.pbm
-do 
-        ffmpeg -hide_banner -loglevel error -i "$f" -vf "scale=$1x$2:flags=neighbor" "$(basename "$f" .pbm).png"
-done
-rm *.pbm
-ffmpeg -hide_banner -loglevel error -f image2 -i %05d.png -vf fps=24 "conways_game_of_life.gif" 
-rm *.png
+
+ffmpeg -hide_banner -loglevel error -f image2 -i %05d.pbm -vf fps=24 -vf "scale=$1x$2:flags=neighbor" "conways_game_of_life.mkv" 
 ffmpeg -hide_banner -loglevel error -i "heatmap.pgm" -vf "scale=$1x$2:flags=neighbor" "heatmap.png"
 rm "heatmap.pgm"
-
-
+rm *.pbm
